87befc2a30eadf810dbf8404e33b5278fe0d55e9
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-05-31 19:53:56 +0200

    update note-label

--- src/note_label.rs
+++ src/note_label.rs
@@ -12,21 +12,21 @@ pub struct NoteLabel{
 
 impl NoteLabel{
     pub async fn all(pool: web::Data<SqlitePool>) -> Result<Vec<NoteLabel>, Error>{
-        let notes_labels = query_as!(NoteLabel, r#"SELECT id, note_id, label_id FROM noteslabels"#)
+        let notes_labels = query_as!(NoteLabel, r#"SELECT id, note_id, label_id FROM notes_labels"#)
             .fetch_all(pool.get_ref())
             .await?;
         Ok(labels)
     }
 
     pub async fn get(pool: web::Data<SqlitePool>, id: i64) -> Result<NoteLabel, Error>{
-        let note_label = query_as!(NoteLabel, r#"SELECT id, note_id, label_id FROM noteslabels WHERE id=$1"#, id)
+        let note_label = query_as!(NoteLabel, r#"SELECT id, note_id, label_id FROM notes_labels WHERE id=$1"#, id)
             .fetch_one(pool.get_ref())
             .await?;
         Ok(note_label)
     }
 
     pub async fn new(pool: web::Data<SqlitePool>, note_id: i64, label_id: i64) -> Result<NoteLabel, Error>{
-        let id = query("INSERT INTO noteslabels (note_id, label_id) VALUES (?, ?);")
+        let id = query("INSERT INTO notes_labels (note_id, label_id) VALUES (?, ?);")
             .bind(note_id)
             .bind(label_id)
             .execute(pool.get_ref())
