eb488f2112b73dddab2e9a8a3311d3ab93dd6ebe
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-05-31 20:31:09 +0200

    update note

--- migrations/20220531173556_table_notes.up.sql
+++ migrations/20220531173556_table_notes.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS notes(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    category_id INTEGER NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL
 );
--- src/note.rs
+++ src/note.rs
@@ -9,7 +9,6 @@ pub struct Note{
     pub id: i64,
     pub title: String,
     pub body: String,
-    pub category_id: i64,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
 }
@@ -18,29 +17,30 @@ pub struct Note{
 #[serde(rename_all = "camelCase")]
 pub struct NewNote{
     pub title: String,
-    pub body: String,
 }
 
 impl Note{
     pub async fn all(pool: web::Data<SqlitePool>) -> Result<Vec<Note>, Error>{
-        let notes = query_as!(Note, r#"SELECT id, title, body, category_id, created_at, updated_at FROM notes"#)
+        let notes = query_as!(Note, r#"SELECT id, title, body, created_at, updated_at FROM notes"#)
             .fetch_all(pool.get_ref())
             .await?;
         Ok(notes)
     }
 
     pub async fn get(pool: web::Data<SqlitePool>, id: i64) -> Result<Note, Error>{
-        let note = query_as!(Note, r#"SELECT id, title, body, category_id, created_at, updated_at FROM notes WHERE id=$1"#, id)
+        let note = query_as!(Note, r#"SELECT id, title, body, created_at, updated_at FROM notes WHERE id=$1"#, id)
             .fetch_one(pool.get_ref())
             .await?;
         Ok(note)
     }
 
-    pub async fn new(pool: web::Data<SqlitePool>, title: &str, body: &str, category_id: i64, created_at: i64, updated_at: i64) -> Result<Note, Error>{
-        let id = query("INSERT INTO notes (title, body, category_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?);")
+    pub async fn new(pool: web::Data<SqlitePool>, title: &str) -> Result<Note, Error>{
+        let body = "";
+        let created_at = 0;
+        let updated_at = 0;
+        let id = query("INSERT INTO notes (title, body, created_at, updated_at) VALUES (?, ?, ?, ?);")
             .bind(title)
             .bind(body)
-            .bind(category_id)
             .bind(created_at)
             .bind(updated_at)
             .execute(pool.get_ref())
