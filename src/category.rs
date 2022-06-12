855707d57844e07b46ae72958d9f1d722be22623
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-06-12 07:16:35 +0200

    update category

--- src/category.rs
+++ src/category.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use sqlx::{query, query_as, FromRow, Error, postgres::PgPool};
+use sqlx::{query_as, FromRow, Error, PgPool};
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, FromRow, Serialize, Deserialize)]
@@ -15,48 +15,47 @@ pub struct NewCategory{
 
 impl Category{
     pub async fn all(pool: web::Data<PgPool>) -> Result<Vec<Category>, Error>{
-        let categories = query_as!(Category, r#"SELECT id, name FROM categories"#)
+        query_as!(Category, r#"SELECT id, name FROM categories"#)
             .fetch_all(pool.get_ref())
-            .await?;
-        Ok(categories)
+            .await
     }
 
     pub async fn get(pool: web::Data<PgPool>, id: i32) -> Result<Category, Error>{
-        let category = query_as!(Category, r#"SELECT id, name FROM categories WHERE id=$1"#, id)
+        query_as!(Category, r#"SELECT id, name FROM categories WHERE id=$1"#, id)
             .fetch_one(pool.get_ref())
-            .await?;
-        Ok(category)
-    }
-
-    pub async fn get_last_inserted(pool: web::Data<PgPool>) -> Result<Category, Error>{
-        let category = query_as!(Category, r#"SELECT id, name FROM categories WHERE id=(SELECT CURRVAL(PG_GET_SERIAL_SEQUENCE('categories', 'id')))"#)
-            .fetch_one(pool.get_ref())
-            .await?;
-        Ok(category)
+            .await
     }
 
     pub async fn new(pool: web::Data<PgPool>, name: &str) -> Result<Category, Error>{
-        let id = query("INSERT INTO categories (name) VALUES (?);")
-            .bind(name)
-            .execute(pool.get_ref())
-            .await?;
-        Self::get_last_inserted(pool).await
+        query_as!(Category,
+                  r#"INSERT INTO categories (name) VALUES ($1) RETURNING id, name;"#,
+                  name)
+            .fetch_one(pool.get_ref())
+            .await
     }
 
     pub async fn update(pool: web::Data<PgPool>, category: Category) -> Result<Category, Error>{
-        query("UPDATE categories SET name=? WHERE id=?;")
-            .bind(category.name)
-            .bind(category.id)
-            .execute(pool.get_ref())
-            .await?;
-        Self::get(pool, category.id).await
+        query_as!(Category,
+                  r#"UPDATE categories SET name=$2 WHERE id=$1 RETURNING id, name;"#,
+                  category.id,
+                  category.name)
+            .fetch_one(pool.get_ref())
+            .await
     }
 
-    pub async fn delete(pool: web::Data<PgPool>, id: i32) -> Result<String, Error>{
-        query("DELETE FROM categories WHERE id = ?;")
-            .bind(id)
-            .execute(pool.get_ref())
-            .await;
-        Ok("Category deleted".to_string())
+    pub async fn delete(pool: web::Data<PgPool>, id: i32) -> Result<Category, Error>{
+        query_as!(Category,
+                  r#"DELETE FROM categories WHERE id = $1 RETURNING id, name;"#,
+                  id)
+            .fetch_one(pool.get_ref())
+            .await
+    }
+
+    pub async fn get_categories_for_note(pool: web::Data<PgPool>, note_id: i32) -> Result<Vec<Category>, Error>{
+        query_as!(Category,
+                  r#"SELECT c.id, c.name FROM categories c INNER JOIN notes_categories nc ON nc.category_id = c.id AND note_id = $1"#,
+                  note_id)
+            .fetch_all(pool.get_ref())
+            .await
     }
 }
