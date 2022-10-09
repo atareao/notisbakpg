b8c2ae9b83b03a7c378037c1e3ef94479807b875
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-09 09:08:14 +0200

    change user

--- migrations/20220618190020_table_users.up.sql
+++ migrations/20220618190020_table_users.up.sql
@@ -1,11 +1,9 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    token TEXT NOT NULL,
     email TEXT NOT NULL,
+    password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    UNIQUE (email),
-    UNIQUE (token)
+    UNIQUE (email)
 );
