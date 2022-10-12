d053e0599679a85c1f4c90c84834b0d9cb2983a1
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-12 20:30:11 +0200

    add claims to utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,42 @@
+use serde::{Serialize, Deserialize};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
-use jsonwebtoken::{decode, decode_header, DecodingKey, Validation};
-use crate::routes::users::Claims;
+use jsonwebtoken::{encode, decode, decode_header, EncodingKey, DecodingKey,
+                   Header, Validation};
+use std::env;
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    pub sub: String,
+    pub exp: usize,
+}
 
+impl Claims{
+    pub fn new(index: i32)-> Self{
+        let expiration: usize = env::var("EXPIRATION")
+            .expect("EXPIRATION not set")
+            .parse()
+            .unwrap();
+        Self{
+            sub: index.to_string(),
+            exp: usize::try_from(jsonwebtoken::get_current_timestamp()).unwrap() + expiration,
+        }
+    }
+    pub fn get_token(&self) -> Result<String, std::io::Error>{
+        let secret = env::var("SECRET").unwrap();
+        Ok(encode(&Header::default(), &self, &EncodingKey::from_secret(secret.as_ref())).unwrap())
 
-pub fn test(credentials: BearerAuth){
-    eprintln!("{}", credentials.token());
-    let decoded = decode::<Claims>(credentials.token(), &DecodingKey::from_secret("SECRETO".as_ref()), &Validation::default());
-    let header = decode_header(credentials.token()).unwrap();
-    let jwt = header.jwk;
-    eprintln!("BearerAuth {:?}", credentials);
-    eprintln!("BearerAuth {:?}", decoded);
-    eprintln!("Sub: {}", decoded.unwrap().claims.sub);
-    eprintln!("decoded {:?}", jwt);
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response{
+    code: String,
+    message: String,
+    token: Option<String>,
+}
+
+pub fn get_user_id_from_token(credentials: BearerAuth) -> Result<i32, std::io::Error>{
+    let secret = env::var("SECRET").unwrap();
+    let decoded = decode::<Claims>(credentials.token(), &DecodingKey::from_secret(secret.as_bytes()), &Validation::default());
+    Ok(decoded.unwrap().claims.sub.parse().unwrap())
 }
