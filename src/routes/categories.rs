bbcf1d0a90ef85de3248298e6a7fb53b00eb144c
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-18 06:39:37 +0200

    improve error return

--- src/routes/categories.rs
+++ src/routes/categories.rs
@@ -15,12 +15,16 @@ use crate::model::{category::{Category, NewCategory}, claims::Claims};
 )]
 #[post("/v1/categories")]
 pub async fn create_category(pool: web::Data<PgPool>, category: web::Json<NewCategory>, credentials: BearerAuth) -> Result<HttpResponse, Error>{
-    let user_id = Claims::get_index(credentials).unwrap();
-    let name = category.into_inner().name;
-    Category::new(pool, &name, user_id)
-       .await
-       .map(|category| HttpResponse::Created().json(category))
-       .map_err(|_| ErrorNotFound("Not found"))
+    match Claims::get_index(credentials) {
+        Ok(user_id) => {
+            let name = category.into_inner().name;
+            Category::new(pool, &name, user_id)
+                .await
+                .map(|category| HttpResponse::Ok().json(category))
+                .map_err(|e| actix_web::error::ErrorConflict(e))
+        },
+        Err(e) => Err(actix_web::error::ErrorUnauthorized(e)),
+    }
 }
 
 #[utoipa::path(
@@ -35,13 +39,17 @@ pub async fn create_category(pool: web::Data<PgPool>, category: web::Json<NewCat
     tag = "categories",
 )]
 #[get("/v1/categories/{id}")]
-pub async fn read_category(pool: web::Data<PgPool>, path: web::Path<i32>, credentials: BearerAuth)->Result<HttpResponse, Error>{
-    let user_id = Claims::get_index(credentials).unwrap();
-    let id = path.into_inner();
-    Category::get(pool, id, user_id)
-       .await
-       .map(|category| HttpResponse::Ok().json(category))
-       .map_err(|_| ErrorNotFound("Not found"))
+pub async fn read_category(pool: web::Data<PgPool>, path: web::Path<i32>, credentials: BearerAuth)->Result<HttpResponse, actix_web::Error>{
+    match Claims::get_index(credentials) {
+        Ok(user_id) => {
+            let id = path.into_inner();
+            Category::get(pool, id, user_id)
+                .await
+                .map(|category| HttpResponse::Ok().json(category))
+                .map_err(|e| actix_web::error::ErrorNotFound(e))
+        },
+        Err(e) => Err(actix_web::error::ErrorUnauthorized(e)),
+    }
 }
 
 #[utoipa::path(
@@ -53,11 +61,15 @@ pub async fn read_category(pool: web::Data<PgPool>, path: web::Path<i32>, creden
 )]
 #[get("/v1/categories")]
 pub async fn read_categories(pool: web::Data<PgPool>, credentials: BearerAuth) -> Result<HttpResponse, Error>{
-    let user_id = Claims::get_index(credentials).unwrap();
-    Category::all(pool, user_id)
-       .await
-       .map(|some_categories| HttpResponse::Ok().json(some_categories))
-       .map_err(|_| ErrorNotFound("Not found"))
+    match Claims::get_index(credentials) {
+        Ok(user_id) => {
+            Category::all(pool, user_id)
+               .await
+               .map(|some_categories| HttpResponse::Ok().json(some_categories))
+               .map_err(|e| actix_web::error::ErrorNotFound(e))
+        },
+        Err(e) => Err(actix_web::error::ErrorUnauthorized(e)),
+    }
 }
 
 #[utoipa::path(
@@ -70,11 +82,16 @@ pub async fn read_categories(pool: web::Data<PgPool>, credentials: BearerAuth) -
     tag = "categories",
 )]
 #[put("/v1/categories")]
-pub async fn update_category(pool: web::Data<PgPool>, category: web::Json<Category>) -> Result<HttpResponse, Error>{
-    Category::update(pool, category.into_inner())
-       .await
-       .map(|category| HttpResponse::Ok().json(category))
-       .map_err(|_| ErrorNotFound("Not found"))
+pub async fn update_category(pool: web::Data<PgPool>, category: web::Json<Category>, credentials: BearerAuth) -> Result<HttpResponse, Error>{
+    match Claims::get_index(credentials) {
+        Ok(_user_id) => {
+            Category::update(pool, category.into_inner())
+               .await
+               .map(|category| HttpResponse::Ok().json(category))
+               .map_err(|e| actix_web::error::ErrorConflict(e))
+        },
+        Err(e) => Err(actix_web::error::ErrorUnauthorized(e)),
+    }
 }
 
 #[utoipa::path(
@@ -88,10 +105,15 @@ pub async fn update_category(pool: web::Data<PgPool>, category: web::Json<Catego
     tag = "categories",
 )]
 #[delete("/v1/categories/{id}")]
-pub async fn delete_category(pool: web::Data<PgPool>, path: web::Path<i32>)->Result<HttpResponse, Error>{
-    let id = path.into_inner();
-    Category::delete(pool, id)
-       .await
-       .map(|category| HttpResponse::Ok().json(category))
-       .map_err(|_| ErrorNotFound("Not found"))
+pub async fn delete_category(pool: web::Data<PgPool>, path: web::Path<i32>, credentials: BearerAuth)->Result<HttpResponse, Error>{
+    match Claims::get_index(credentials) {
+        Ok(_user_id) => {
+            let id = path.into_inner();
+            Category::delete(pool, id)
+               .await
+               .map(|category| HttpResponse::Ok().json(category))
+               .map_err(|e| ErrorNotFound(e))
+        },
+        Err(e) => Err(actix_web::error::ErrorUnauthorized(e)),
+    }
 }
